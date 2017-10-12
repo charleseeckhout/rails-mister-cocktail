071afffd152b70a0d3f5fda5025b094d29b41265
charles eeckhout <charleseeckhout@gmail.com>
2017-10-12 15:52:34 +0900

    create a part of controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,34 @@
 class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+  #Create a restaurant with attributes from the params
+    @cocktail = Cocktail.new(cocktail_params)
+  # save it
+    if @cocktail.save
+  # redirect
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new #render 'new.html.erb'
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,24 @@
 class DosesController < ApplicationController
+
+before_action :set_restaurant, only: [:new, :create]
+
+  def create
+  @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktail/show'
+      end
+  end
+
+   private
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def doses_params
+    params.require(:dose).permit(:description)
+  end
 end
+
