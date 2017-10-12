88e4f516a0a823d0081f50b5a06a47e6f47f2a71
charles eeckhout <charleseeckhout@gmail.com>
2017-10-12 14:20:51 +0900

    validation

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,11 @@
 class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
+
+  # validates :ingredient, presence: true
+  # validates :ingredient, uniqueness: true
+  # validates :cocktail, presence: true
+  # validates :cocktail, uniqueness: true
+
+  validates_uniqueness_of :ingredient_id, :scope => [:cocktail_id]
 end
